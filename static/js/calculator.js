d43bbba63a7dd146344dc3386821acfdd23aaaa9
tumrod <tipparat.umrod@utexas.edu>
2015-09-27 06:25:09 -0500

    workable projects

--- static/js/calculator.js
+++ static/js/calculator.js
@@ -1,13 +1,50 @@
-$(document).ready(function() {
+$(document).ready(function (){
+    var $result= $('#grade');
+    var totalGradePoint = 0;
 
-    //var courseName = $(#course-name).val()
-    $('#gradeForm').submit(function() {
-        var $inputs = $('#course-name :input');
+    $('#myForm').submit(function() {
+    // get all the inputs into an array.
+    var $inputs = $('#myForm :input');
 
-        var values = {};
-        $inputs.each(function() {
-            values[this.name] = $(this).val();
-        });
+    // not sure if you wanted this, but I thought I'd add it.
+    // get an associative array of just the values.
+    var values = {};
+    $inputs.each(function() {
+        values[this.name] = $(this).val();
     });
+
+    });
+
+    var totalCreditPoint += creditHours;
+    var gpaCalc(function(creditHours, grade) {
+        var gradeNum = 0.00;
+        if (grade === 'A') {
+            gradeNum = 4.00;
+        } else if (grade === 'A-') {
+            gradeNum = 3.67;
+        } else if (grade === 'B+') {
+            gradeNum = 3.33;
+        } else if (grade === 'B') {
+            gradeNum = 3.00;
+        } else if (grade === 'B-') {
+            gradeNum = 2.67;
+        } else if (grade === 'C+') {
+            gradeNum = 2.33;
+        } else if (grade === 'C') {
+            gradeNum = 2.00;
+        } else if (grade === 'C-') {
+            gradeNum = 1.67;
+        } else if (grade === 'D+') {
+            gradeNum = 1.33;
+        } else if (grade === 'D') {
+            gradeNum = 1.00;
+        } else if (grade === 'D-') {
+            gradeNum = 0.67;
+        } 
+
+        return gradeNum * creditHours;
+    })
+    totalGradePoint += gpaCalc('#credit', '#grade');
+    var finalGpa = totalGradePoint / totalCreditPoint;
 
 });
--- uScheme.py
+++ uScheme.py
@@ -1,5 +1,8 @@
 from flask import *
 import jinja2, os
+from gpaCalc import *
+import re
+
 app = Flask(__name__)
 
 
@@ -8,46 +11,70 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), a
 
 
 def render_str(template, **params):
-    t = jinja_env.get_template(template)
-    return t.render(params)
+	t = jinja_env.get_template(template)
+	return t.render(params)
 
 def render(template, **params):
-    return render_template(render_str(template, **params))
+	return render_template(render_str(template, **params))
 
 @app.route('/')
 def index():
-    submit = request.args.get("in")
-    email = request.args.get("email")
-    password = request.args.get("password")
-    if submit:
-        return redirect(url_for('grade'))
-    return render_template('homepage.html')
+	submit = request.args.get("in")
+	email = request.args.get("email")
+	password = request.args.get("password")
+	print(email)
 
-@app.route('/grade')
-def grade():
+	if submit:
+		return redirect(url_for('landingPage'))
+	return render_template('homepage.html')
 
-    return render_template('grade.html')
-'''
 @app.route('/main')
 def landingPage():
-    return render_template('landingPage.html')
+	return render_template('landingpage.html')
 
+@app.route('/gpa')
+def gpa():
+	submit = request.args.get("submit-gpa")
+	grade = request.values.getlist('grade')
+	creditHr = request.values.getlist('creditHr')
+
+	length = len(creditHr)
+	grade_point = 0
+	total_hour = 0
+	for i in range(length):
+		if grade[i] == '' or creditHr[i] == '':
+			continue
+		print (grade[i], creditHr[i])
+		grade_point += float(grade[i]) * float(creditHr[i])
+		total_hour += float(creditHr[i])
+
+
+	print (grade_point, total_hour)
+	if total_hour and grade_point:
+		gpa = grade_point / total_hour
+
+		# pair = list(zip(creditHr,grade))
+		gpa = round(gpa,2)
+
+		return render_template('gpa.html', gpa=gpa)
+	return render_template('gpa.html')
+'''
 @app.route('/schedule')
 def schedule():
-    return render_template('schedule.html')
+	return render_template('schedule.html')
 
 @app.route('/map')
 def map():
-    return render_template('map.html')
+	return render_template('map.html')
 
 @app.route('/class')
 def myclass():
-    course_data = Course.get_course_name()
-    return render_template('class.html', course_data)
-
-@app.route('/gpa')
+	course_data = Course.get_course_name()
+	return render_template('class.html', course_data)
+@app.route('/grade')
 def grade():
-    return render_template('gpa.html')
+	return render_template('grade.html')
+
 '''
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
