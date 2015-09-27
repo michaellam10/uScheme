3324f3c1c0623aeac09fa2342dddf297b6f25e2f
tumrod <tipparat.umrod@utexas.edu>
2015-09-26 19:04:16 -0500

    added more routes

--- uScheme.py
+++ uScheme.py
@@ -5,5 +5,21 @@ app = Flask(__name__)
 def index():
     return render_template('homepage.html')
 
+@app.route('/schedule')
+def schedule():
+    return render_template('schedule.html')
+
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
+@app.route('/class')
+def myclass():
+    return render_template('class.html')
+
+@app.route('/grade-gpa-prediction')
+def grade():
+    return render_template('grade.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
